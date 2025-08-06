c2cd4f5e567b94f59609f27e4ed3f436df95024e
rafi <rafiahmedrabby282@gmail.com>
2025-08-07 00:02:12 +0600

    changed repo

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
 import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
-import img from "/rafi.jpg";
+import img from "/rafi.png";
 import "animate.css";
 import { motion } from "framer-motion";
 import Button from "./Button";
@@ -11,24 +11,12 @@ const Banner = () => {
       id="banner"
       className="grid md:grid-cols-2 items-center rounded-lg shadow-lg gap-6 sm:gap-8 pb-8 sm:pb-12 md:pb-20 "
     >
-      <div className="space-y-4 md:space-y-8 text-center sm:text-left ">
+      <div className="space-y-4 md:space-y-8 text-center sm:text-left">
         <h1 className="text-3xl md:text-5xl xl:text-8xl font-bold text-[#E80F88] ">
           Rafi Ahmed
         </h1>
-        <p   className="text-2xl md:text-4xl lg:text-5xl font-semibold flex items-center justify-center "><A4Animation/></p>
+        <p className="text-2xl md:text-4xl lg:text-5xl font-semibold flex items-center justify-center "><A4Animation /></p>
 
-        {/* <motion.p
-          className="text-2xl md:text-4xl lg:text-5xl font-semibold"
-          animate={{ x: [0, 100, 0] }}
-          transition={{
-            duration: 5,
-            delay: 2,
-            ease: "linear",
-            times: [0, 0.3, 1],
-          }}
-        >
-          Frontend Developer
-        </motion.p> */}
 
         <div className="flex justify-center sm:justify-start gap-6 text-3xl md:text-4xl">
           <a href="https://www.facebook.com/rafiahmed.rabby.3/" target="_blank">
@@ -61,30 +49,30 @@ const Banner = () => {
         </div>
       </div>
 
-       <motion.div
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.8 }}
-          className="w-full  flex justify-center mb-8 lg:mb-0 conic "
-        >
-          <div className="relative">
-            {/* Background decoration */}
-            <div className="absolute -z-10 w-72 h-72 md:w-96 md:h-96 rounded-full bg-gradient-to-br from-primary/10 to-purple/10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
-            <motion.div
-              whileHover={{ scale: 1.05, rotate: -2 }}
-              transition={{ type: "spring", stiffness: 300 }}
-              className="relative z-10"
-            >
-              <div className="absolute inset-0 bg-gradient-to-br from-primary to-purple rounded-2xl rotate-6 transform scale-95 opacity-20 blur-lg"></div>
-              <img
-                className="h-64 w-64 md:h-80 md:w-80 lg:h-96 lg:w-96 object-cover rounded-2xl shadow-2xl"
-                src={img}
-                alt="Rafi Ahmed - Frontend Developer"
-              />
-            
-            </motion.div>
-          </div>
-        </motion.div>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.8 }}
+        className="w-full  flex justify-center mb-8 lg:mb-0 conicc "
+      >
+        <div className="relative">
+          {/* Background decoration */}
+          {/* <div className="absolute -z-10 w-72 h-72 md:w-96 md:h-60 rounded-full bg-gradient-to-br from-primary/10 to-purple/10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div> */}
+          <motion.div
+            whileHover={{ scale: 1.05, rotate: -2 }}
+            transition={{ type: "spring", stiffness: 300 }}
+            className="relative z-10 conic"
+          >
+            {/* <div className="absolute inset-0 bg-gradient-to-br from-primary to-purple rounded-2xl rotate-6 transform scale-95 opacity-20 blur-lg"></div> */}
+            <img
+              className="object-cover rounded-2xl shadow-2xl  w-64 h-60 md:w-80 md:h-60 lg:w-[28rem] lg:h-[26rem]"
+              src={img}
+              alt="Rafi Ahmed - Frontend Developer"
+            />
+
+          </motion.div>
+        </div>
+      </motion.div>
 
       {/* <div className="flex justify-center ">
         <motion.img
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({text, size}) => {
       <button>
         <a
           download={true}
-          href="Resume_Of_Rafi_Ahmed.pdf"
+          href="Resume_Of_Rafi_Ahmed_full_stack_developer.pdf"
           className={ ` border-[#E80F88] md:text-3xl font-semibold px-1 md:px-5 py-2 rounded-lg hover:bg-[#E80F88] border-l-[#E80F88] border-l-[5px] hover:text-white transition duration-200 shadow shadow-[#E80F88] animate__fadeInUp animate__animated animate__delay-2s  animate__slower `}
         >
           {text}
